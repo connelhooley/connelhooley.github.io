e48d54379fe2f37a86a494278ebd4f02caed7135
Connel Hooley <me@connelhooley.uk>
2022-05-25 22:49:55 +0000

    Started again with blank gatsby template with tailwind set up

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+import { Layout } from "../components/Layout";
+
+const IndexPage = () => {
+  return (
+    <Layout>
+      Todo
+    </Layout>
+  );
+};
+
+export default IndexPage;
---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+const color = require('color')
+
+const lighen = (clr, val) => color(clr).lighten(val).rgb().string();
+const darken = (clr, val) => color(clr).darken(val).rgb().string();
+
+const primaryColor = "#f8bb15";
+
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          "light": lighen(primaryColor, .2),
+          "DEFAULT": primaryColor,
+          "dark": darken(primaryColor, .2),
+        },
+      },      
+      fontFamily: {
+        "logo": ["Slabo 27px", ...defaultTheme.fontFamily.serif],
+        "sans": ["Open Sans", ...defaultTheme.fontFamily.sans],
+        "serif": ["Bree Serif", ...defaultTheme.fontFamily.serif],
+        "mono": ["Source Code Pro", ...defaultTheme.fontFamily.mono],
+      },
+    },
+  },
+  plugins: [
+    require("@tailwindcss/typography"),
+  ],
+};
