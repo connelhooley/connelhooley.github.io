498c8e9c9cb53947358a4f32613b8b50ee836baa
Connel Hooley <me@connelhooley.uk>
2022-05-30 21:48:00 +0000

    Fixed active highlighting

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -60,6 +60,7 @@ const PageLink = ({ children, ...props }) => {
     <Link
       className="px-6 py-3 sm:p-6 text-right sm:text-center sm:flex items-center justify-center transition-colors duration-400 hover:text-primary text-md"
       activeClassName="text-primary font-bold sm:shadow-primary-b"
+      partiallyActive
       {...props}
     >
       {children}
