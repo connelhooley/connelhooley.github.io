fc6d027d6f448524bbfc60054f74f4e4882d836b
Connel Hooley <me@connelhooley.uk>
2022-06-10 15:59:14 +0000

    Refactored 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,22 +4,41 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHome } from "@fortawesome/free-solid-svg-icons";
 
 import { Layout } from "../components/Layout";
-import { Header, Container, Section, Title } from "../components/Page";
-import { Button } from "../components/Button";
+import { Header } from "../components/Page";
 
 const NotFoundPage = () => {
   return (
     <Layout>
-      <Header className="text-center">
-        <h1 className="font-logo font-black text-white tracking-widest sm:tracking-[1.5rem] text-8xl mb-12">{"{"}4.0.4{"}"}</h1>
-        <p><Link className="inline-block p-4 text-center bg-black text-white hover:text-primary transition-colors duration-400" to="/"><FontAwesomeIcon icon={faHome} />&nbsp;Home</Link></p>
-      </Header>
-      <Container>
-        <Section>
-        </Section>
-      </Container>
+      <NotFoundHeader>
+        <NotFoundTitle>{"{"}4.0.4{"}"}</NotFoundTitle>
+        <NotFoundButton to="/"><FontAwesomeIcon icon={faHome} />&nbsp;Home</NotFoundButton>
+      </NotFoundHeader>
     </Layout>
   )
 };
 
-export default NotFoundPage
+const NotFoundHeader = ({ children }) => {
+  return (
+    <Header className="text-center">
+      {children}
+    </Header>
+  )
+};
+
+const NotFoundTitle = ({ children }) => {
+  return (
+    <h1 className="font-logo font-black text-white tracking-widest sm:tracking-[1.5rem] text-8xl mb-12">
+      {children}
+    </h1>
+  )
+};
+
+const NotFoundButton = ({ children, ...props }) => {
+  return (
+    <Link className="inline-block p-4 text-center bg-black text-white hover:text-primary transition-colors duration-400" {...props}>
+      {children}
+    </Link>
+  )
+};
+
+export default NotFoundPage;
