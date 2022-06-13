0519f32dbe705fd31b8045466376f4d0e44b94bf
Connel Hooley <me@connelhooley.uk>
2022-06-13 10:03:31 +0000

    Added SO link

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,7 @@
 import "./src/styles/global.css";
 import "./src/styles/prism.css";
 
+// https://stackoverflow.com/a/59429852/310098
 // https://github.com/FortAwesome/react-fontawesome/issues/134
 // The following import prevents a Font Awesome icon server-side rendering bug,
 // where the icons flash from a very large icon down to a properly sized one:
