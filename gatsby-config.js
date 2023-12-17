226e799d9965549f31cf8dfe219ab8e23aef46d6
Connel Hooley <me@connelhooley.uk>
2023-12-17 15:39:16 +0000

    Up CV version

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,7 +11,7 @@ module.exports = {
       linkedIn: "connelhooley",
       mastodon: "https://mastodon.social/@connel",
     },
-    cvVersion: 4, // Used to cache bust CV
+    cvVersion: 5, // Used to cache bust CV
   },
   plugins: [
     {
