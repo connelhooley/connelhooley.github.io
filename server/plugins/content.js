8337c93756d3d55391a00d19480183eee3d30a0e
Connel Hooley <connelhooley@gmail.com>
2024-05-05 23:00:50 +0100

    Date fix

--- server/plugins/content.js
+++ server/plugins/content.js
@@ -4,7 +4,7 @@ export default defineNitroPlugin((nitroApp) => {
       const segments = file._path.split("/");
       if (segments.length > 5) {
         const [year, month, day] =  segments.slice(2,5);
-        file.date = new Date(parseInt(year), parseInt(month), parseInt(day));
+        file.date = new Date(parseInt(year), parseInt(month) - 1, parseInt(day));
       }
     }
     return file;
