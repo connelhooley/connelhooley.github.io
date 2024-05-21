e44ae37569d5a56dbfe4e93ff96ea1c6e365073d
Connel Hooley <connelhooley@gmail.com>
2024-05-21 22:15:25 +0100

    Test code

---
+++ components/Slides/Slide.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+defineProps<{ active?: boolean }>();
+</script>
+
+<template>
+  <div :class="['h-screen w-screen hidden', { 'block': active }]">
+    <slot />
+  </div>
+</template>
--- pages/slides/[...slug].vue
+++ pages/slides/[...slug].vue
@@ -1,9 +1,19 @@
 <script lang="ts" setup>
+import { SlidesTest } from '#components';
+
 definePageMeta({ layout: false });
+
+const route = useRoute();
+const { data } = await useAsyncData(() => queryContent(route.path).findOne());
+const components = {
+  "hr": SlidesTest,
+}
 </script>
 
 <template>
   <main>
-    <ContentDoc />
+    <div>
+      <ContentRendererMarkdown :value="data" :components="components" />
+    </div>
   </main>
 </template>
