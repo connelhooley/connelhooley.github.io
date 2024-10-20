a917f52cdeaec2dde050de8c12d1a1418f9b18c6
Connel Hooley <me@connelhooley.uk>
2024-10-20 11:15:05 +0000

    Nest load content functions

--- build.js
+++ build.js
@@ -60,155 +60,6 @@ await rm(distDir, { recursive: true, force: true });
 await mkdir(distDir);
 console.log("Cleared dist");
 
-const loadBlogContent = async () => {
-  console.log("Loading blog content");
-  const blog = {
-    posts: [],
-    languages: new Set(),
-    technologies: new Set(),
-  };
-  const srcMdFilePaths = await glob(srcDir + "/content/blog/*/*/*/*/index.md");
-  await mermaidStart();
-  await Promise.all(srcMdFilePaths.map(async srcFilePath => {
-    const srcFilePathParsed = path.parse(srcFilePath);
-    const parsedFile = await unified()
-      .use(() => (_, file) => matter(file))
-      .use(remarkParse)
-      .use(remarkFrontmatter)
-      .use(remarkGfm)
-      .use(remarkRehype)
-      .use(rehypeSlug)
-      .use(rehypeAutolinkHeadings, {
-        behavior: "append",
-        headingProperties: { "class": "heading" },
-        properties: { "class": "link" },
-        content(node) {
-          return [
-            h("span", [
-              h("i.fa-solid fa-link", { "aria-hidden": "true" }),
-              h("span", `Go to ${toString(node)} section`),
-            ]),
-          ];
-        },
-      })
-      .use(rehypeToc)
-      .use(rehypeHighlight, {
-        plainText: ["mermaid"],
-      })
-      .use(rehypeMermaid)
-      .use(rehypeStringify)
-      .process(await readFile(srcFilePath));
-    const data = parsedFile.data.matter;
-    const relativePath = path.relative(path.join(srcDir, "content"), srcFilePathParsed.dir);
-    const [year, month, day] = relativePath.split(path.sep).slice(1, 4);
-    data.date = new Date(`${year}-${month}-${day}T${data.time}Z`);
-    if (data.draft) return;
-    const tempFilePath = path.join(
-      tempDir,
-      relativePath,
-      "index.html");
-    const distFilePath = path.join(
-      distDir,
-      relativePath,
-      "index.html");
-    const route = path.join(
-      "/",
-      relativePath,
-      "/");
-    await mkdir(path.dirname(tempFilePath), { recursive: true });
-    await writeFile(tempFilePath, parsedFile.toString("utf-8"));
-    blog.posts.push({
-      route,
-      meta: {
-        srcFilePath,
-        tempFilePath,
-        distFilePath,
-      },
-      data,
-    });
-    data?.languages?.forEach(language => blog.languages.add(language));
-    data?.technologies?.forEach(technology => blog.technologies.add(technology));
-  }));
-  mermaidStop();
-  blog.posts.sort((a, b) => {
-    const dateA = new Date(a.data.date);
-    const dateB = new Date(b.data.date);
-    if (dateA < dateB) return 1;
-    if (dateA > dateB) return -1;
-    return 0;
-  });
-  blog.posts.forEach((post, index) => {
-    if (index > 0) {
-      const { prev, next, ...nextPost } = blog.posts[index - 1]
-      post.next = nextPost;
-    }
-    if (index + 1 <= blog.posts.length - 1) {
-      const { prev, next, ...prevPost } = blog.posts[index + 1]
-      post.prev = prevPost;
-    }
-  });
-  console.log("Loaded blog content");
-  return blog;
-};
-
-const loadExperienceContent = async () => {
-  console.log("Loading experience content");
-  const srcMdFilePaths = await glob(srcDir + "/content/experience/**/*.md");
-  const experiences = await Promise.all(srcMdFilePaths.map(async srcFilePath => {
-    const parsedFile = await unified()
-      .use(() => (_, file) => matter(file))
-      .use(remarkParse)
-      .use(remarkFrontmatter)
-      .use(remarkGfm)
-      .use(remarkRehype)
-      .use(rehypeFormat)
-      .use(rehypeStringify)
-      .process(await readFile(srcFilePath));
-    return {
-      content: parsedFile.toString("utf-8"),
-      data: parsedFile.data.matter,
-    };
-  }));
-  experiences.sort((a, b) => {
-    const startA = a.data.start;
-    const startB = b.data.start;
-    if (startA < startB) return 1;
-    if (startA > startB) return -1;
-    return 0;
-  });
-  console.log("Loaded experience content");
-  return experiences;
-};
-
-const loadProjectsContent = async () => {
-  console.log("Loading projects content");
-  const srcMdFilePaths = await glob(srcDir + "/content/projects/**/*.md");
-  const projects = await Promise.all(srcMdFilePaths.map(async srcFilePath => {
-    const parsedFile = await unified()
-      .use(() => (_, file) => matter(file))
-      .use(remarkParse)
-      .use(remarkFrontmatter)
-      .use(remarkGfm)
-      .use(remarkRehype)
-      .use(rehypeFormat)
-      .use(rehypeStringify)
-      .process(await readFile(srcFilePath));
-    return {
-      content: parsedFile.toString("utf-8"),
-      data: parsedFile.data.matter,
-    };
-  }));
-  projects.sort((a, b) => {
-    const orderA = new Date(a.data.order);
-    const orderB = new Date(b.data.order);
-    if (orderA < orderB) return -1;
-    if (orderA > orderB) return 1;
-    return 0;
-  });
-  console.log("Loaded projects content");
-  return projects;
-};
-
 const buildCss = async () => {
   console.log("Building CSS");
   const srcFilePath = path.join(
@@ -291,6 +142,35 @@ const renderHome = async () => {
 };
 
 const renderExperience = async () => {
+  const loadExperienceContent = async () => {
+    console.log("Loading experience content");
+    const srcMdFilePaths = await glob(srcDir + "/content/experience/**/*.md");
+    const experiences = await Promise.all(srcMdFilePaths.map(async srcFilePath => {
+      const parsedFile = await unified()
+        .use(() => (_, file) => matter(file))
+        .use(remarkParse)
+        .use(remarkFrontmatter)
+        .use(remarkGfm)
+        .use(remarkRehype)
+        .use(rehypeFormat)
+        .use(rehypeStringify)
+        .process(await readFile(srcFilePath));
+      return {
+        content: parsedFile.toString("utf-8"),
+        data: parsedFile.data.matter,
+      };
+    }));
+    experiences.sort((a, b) => {
+      const startA = a.data.start;
+      const startB = b.data.start;
+      if (startA < startB) return 1;
+      if (startA > startB) return -1;
+      return 0;
+    });
+    console.log("Loaded experience content");
+    return experiences;
+  };
+
   const experiences = await loadExperienceContent();
   console.log("Rendering experience page");
   const renderedTemplate = await eta.renderAsync("experience", {
@@ -310,6 +190,35 @@ const renderExperience = async () => {
 };
 
 const renderProjects = async () => {
+  const loadProjectsContent = async () => {
+    console.log("Loading projects content");
+    const srcMdFilePaths = await glob(srcDir + "/content/projects/**/*.md");
+    const projects = await Promise.all(srcMdFilePaths.map(async srcFilePath => {
+      const parsedFile = await unified()
+        .use(() => (_, file) => matter(file))
+        .use(remarkParse)
+        .use(remarkFrontmatter)
+        .use(remarkGfm)
+        .use(remarkRehype)
+        .use(rehypeFormat)
+        .use(rehypeStringify)
+        .process(await readFile(srcFilePath));
+      return {
+        content: parsedFile.toString("utf-8"),
+        data: parsedFile.data.matter,
+      };
+    }));
+    projects.sort((a, b) => {
+      const orderA = new Date(a.data.order);
+      const orderB = new Date(b.data.order);
+      if (orderA < orderB) return -1;
+      if (orderA > orderB) return 1;
+      return 0;
+    });
+    console.log("Loaded projects content");
+    return projects;
+  };
+
   const projects = await loadProjectsContent();
   console.log("Rendering projects page");
   const renderedTemplate = await eta.renderAsync("projects", {
@@ -329,6 +238,96 @@ const renderProjects = async () => {
 };
 
 const renderBlog = async () => {
+  const loadBlogContent = async () => {
+    console.log("Loading blog content");
+    const blog = {
+      posts: [],
+      languages: new Set(),
+      technologies: new Set(),
+    };
+    const srcMdFilePaths = await glob(srcDir + "/content/blog/*/*/*/*/index.md");
+    await mermaidStart();
+    await Promise.all(srcMdFilePaths.map(async srcFilePath => {
+      const srcFilePathParsed = path.parse(srcFilePath);
+      const parsedFile = await unified()
+        .use(() => (_, file) => matter(file))
+        .use(remarkParse)
+        .use(remarkFrontmatter)
+        .use(remarkGfm)
+        .use(remarkRehype)
+        .use(rehypeSlug)
+        .use(rehypeAutolinkHeadings, {
+          behavior: "append",
+          headingProperties: { "class": "heading" },
+          properties: { "class": "link" },
+          content(node) {
+            return [
+              h("span", [
+                h("i.fa-solid fa-link", { "aria-hidden": "true" }),
+                h("span", `Go to ${toString(node)} section`),
+              ]),
+            ];
+          },
+        })
+        .use(rehypeToc)
+        .use(rehypeHighlight, {
+          plainText: ["mermaid"],
+        })
+        .use(rehypeMermaid)
+        .use(rehypeStringify)
+        .process(await readFile(srcFilePath));
+      const data = parsedFile.data.matter;
+      const relativePath = path.relative(path.join(srcDir, "content"), srcFilePathParsed.dir);
+      const [year, month, day] = relativePath.split(path.sep).slice(1, 4);
+      data.date = new Date(`${year}-${month}-${day}T${data.time}Z`);
+      if (data.draft) return;
+      const tempFilePath = path.join(
+        tempDir,
+        relativePath,
+        "index.html");
+      const distFilePath = path.join(
+        distDir,
+        relativePath,
+        "index.html");
+      const route = path.join(
+        "/",
+        relativePath,
+        "/");
+      await mkdir(path.dirname(tempFilePath), { recursive: true });
+      await writeFile(tempFilePath, parsedFile.toString("utf-8"));
+      blog.posts.push({
+        route,
+        meta: {
+          srcFilePath,
+          tempFilePath,
+          distFilePath,
+        },
+        data,
+      });
+      data?.languages?.forEach(language => blog.languages.add(language));
+      data?.technologies?.forEach(technology => blog.technologies.add(technology));
+    }));
+    mermaidStop();
+    blog.posts.sort((a, b) => {
+      const dateA = new Date(a.data.date);
+      const dateB = new Date(b.data.date);
+      if (dateA < dateB) return 1;
+      if (dateA > dateB) return -1;
+      return 0;
+    });
+    blog.posts.forEach((post, index) => {
+      if (index > 0) {
+        const { prev, next, ...nextPost } = blog.posts[index - 1]
+        post.next = nextPost;
+      }
+      if (index + 1 <= blog.posts.length - 1) {
+        const { prev, next, ...prevPost } = blog.posts[index + 1]
+        post.prev = prevPost;
+      }
+    });
+    console.log("Loaded blog content");
+    return blog;
+  };
   const renderBlogPosts = async ({ posts }) => {
     console.log("Rendering blog posts");
     await Promise.all(posts.map(async post => {
