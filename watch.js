0506ebba7cc9efdb12c5a90bd133c72604489770
Connel Hooley <me@connelhooley.uk>
2024-10-21 20:35:45 +0000

    Fixes to watch.js

--- watch.js
+++ watch.js
@@ -14,61 +14,61 @@ import {
 } from "./build.js";
 
 const watchCss = async () => {
-  for await (const change of watch("./src/styles")) {
+  for await (const change of watch("./src/styles", { recursive: true })) {
     await buildCss();
   }
 };
 
 const watchJs = async () => {
-  for await (const change of watch("./src/scripts")) {
+  for await (const change of watch("./src/scripts", { recursive: true })) {
     await buildJs();
   }
 };
 
 const watchStatic = async () => {
-  for await (const change of watch("./src/static")) {
+  for await (const change of watch("./src/static", { recursive: true })) {
     await copyStaticAssets();
   }
 };
 
 const watchTemplates = async () => {
-  for await (const change of watch("./src/templates")) {
-    if (minimatch(change.filename, "./src/templates/layout/default.eta")) {
+  for await (const change of watch("./src/templates", { recursive: true })) {
+    if (minimatch(change.filename, "layout/default.eta")) {
       await Promise.all([
         renderBlog(),
         renderExperience(),
         renderProjects(),
       ]);
-    } else if (minimatch(change.filename, "./src/templates/{blog-post|paged-collection}.eta")) {
+    } else if (minimatch(change.filename, "{blog-post|paged-collection}.eta")) {
       await renderBlog();
-    } else if (minimatch(change.filename, "./src/templates/home.eta")) {
+    } else if (minimatch(change.filename, "home.eta")) {
       await renderHome();
-    } else if (minimatch(change.filename, "./src/template/experience.eta")) {
+    } else if (minimatch(change.filename, "experience.eta")) {
       await renderExperience();
-    } else if (minimatch(change.filename, "./src/templates/projects.eta")) {
+    } else if (minimatch(change.filename, "projects.eta")) {
       await renderProjects();
     }
   }
 };
 
 const watchBlogContent = async () => {
-  for await (const change of watch("./src/content/blog")) {
-    if (minimatch(change.filename, "./src/content/blog/*/*/*/*/index.md")) {
+  for await (const change of watch("./src/content/blog", { recursive: true })) {
+    if (minimatch(change.filename, "*/*/*/*/index.md")) {
       await renderBlog();
-    } else if (minimatch(change.filename, "./src/content/blog/**/*.png")) {
+    } else if (minimatch(change.filename, "**/*.png")) {
       await copyBlogAssets();
     }
   }
 };
 
 const watchExperienceContent = async () => {
-  for await (const change of watch("./src/content/experience")) {
+  for await (const change of watch("./src/content/experience", { recursive: true })) {
     await renderExperience();
   }
 };
 
 const watchProjectsContent = async () => {
-  for await (const change of watch("./src/content/projects")) {
+  for await (const change of watch("./src/content/projects", { recursive: true })) {
     await renderProjects();
   }
 };
