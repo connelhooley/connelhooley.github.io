ad6680e4837dea348621d22042ae8d36bf8351b2
Connel Hooley <me@connelhooley.uk>
2024-10-25 22:10:14 +0000

    Finished blog posts page, started blog post page

--- build.js
+++ build.js
@@ -49,7 +49,7 @@ const defaultRehypeDocumentOptions = {
     "/vendor/dayjs/js/dayjs.min.js",
     "/vendor/dayjs/js/relativeTime.min.js",
     "/js/copy-code.js",
-    "/js/date.js",
+    "/js/relative-date.js",
     "/js/mobile-menu.js"
   ],
 };
@@ -269,7 +269,7 @@ export const renderBlog = async () => {
             return [
               h("span", [
                 h("i.fa-solid fa-link", { "aria-hidden": "true" }),
-                h("span", `Go to ${toString(node)} section`),
+                h("span", { "class": "sr-only" }, `Go to ${toString(node)} section`),
               ]),
             ];
           },
@@ -285,8 +285,6 @@ export const renderBlog = async () => {
       const relativePath = path.relative(path.join(srcDir, "content"), srcFilePathParsed.dir);
       const [year, month, day] = relativePath.split(path.sep).slice(1, 4);
       data.date = new Date(`${year}-${month}-${day}T${data.time}Z`);
-      data.dateFormatted = new Intl.DateTimeFormat('en-GB').format(data.date);
-      data.dateIso = data.date.toISOString();
       if (data.draft) return;
       const tempFilePath = path.join(
         tempDir,
