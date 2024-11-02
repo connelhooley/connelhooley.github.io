695fd16d2abc9a4c8fa9fbc994c9c0d1b3b67ab9
Connel Hooley <me@connelhooley.uk>
2024-11-02 14:15:00 +0000

    Favicon

--- build.js
+++ build.js
@@ -45,9 +45,20 @@ const eta = new Eta({
   cache: true,
   cacheFilepaths: true,
 });
+
 const defaultRehypeDocumentOptions = {
   language: "en-GB",
-  meta: [{ name: "fediverse:creator", content: "@connel@mastodon.social" }],
+  meta: [
+    { name: "fediverse:creator", content: "@connel@mastodon.social" },
+    { name: "apple-mobile-web-app-title", content: "Connel Hooley" },
+  ],
+  link: [
+    { rel: "icon", type: "image/png", href: "/favicon-96x96.png", sizes: "96x96" },
+    { rel: "icon", type: "image/svg+xml", href: "/favicon.svg" },
+    { rel: "shortcut icon", href: "/favicon.ico" },
+    { rel: "apple-touch-icon", type: "image/png", href: "/apple-touch-icon.png", sizes: "180x180" },
+    { rel: "manifest", href: "/site.webmanifest" },
+  ],
   css: [
     "/vendor/highlight.js/css/ir-black.min.css",
     "/vendor/font-awesome/css/fontawesome.min.css",
@@ -570,5 +581,4 @@ console.log("Site built successfully");
 // TODO finish home page
 // TODO finish site-footer
 
-// TODO Favicon
 // TODO Slides
